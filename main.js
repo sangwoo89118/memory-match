d26654f5687ba77fb93020ed2b332e962dca2113
sangwoo89118 <sangwoo89118@gmail.com>
2017-11-06 12:33:58 -0800

    Memory match version1.0 - Sangwoo Kim'

--- main.js
+++ main.js
@@ -78,8 +78,8 @@ function card_clicked() {
                 display_stats();
                 wait1sec();
                 function wait1sec(){
-                    $(first_card_clicked).parent().find('.front').toggleClass('damn');
-                    $(second_card_clicked).parent().find('.front').toggleClass('damn')
+                    $(first_card_clicked).parent().find('.front').toggleClass('spinner');
+                    $(second_card_clicked).parent().find('.front').toggleClass('spinner')
 
                     setTimeout(function (){
                         $(first_card_clicked).parent().find('.front').toggleClass('hidden');
@@ -135,7 +135,6 @@ function moveCard () {
         themeAudio.muted = false;
     }
 
-
     var counterF = null;
     var cardArr =[];
     var bouncerN = true;
@@ -157,7 +156,6 @@ function moveCard () {
     var bouncer2 = true;
     while(bouncer2){
         if(counterN < 18){
-
             var randomNum2 = Math.floor(Math.random() * 18)+1;
             if(elementArr.indexOf(randomNum2) === -1){
                 counterN++;
@@ -167,23 +165,25 @@ function moveCard () {
             bouncer2 = false;
         }
     }
-    console.log(elementArr);
+
+
 
 
     for(var i = 0, k =0; i <cardArr.length , k < elementArr.length; i++, k+=2){
+
         $('#game-area :nth-child(' + elementArr[k] + ') .front img').attr('src', 'images/card'+cardArr[i]+'.png');
         $('#game-area :nth-child(' + elementArr[k+1] + ') .front img').attr('src', 'images/card'+cardArr[i]+'.png');
 
         $('#game-area :nth-child(' + elementArr[k] + ') .front img').parent().parent().css({
-            left: -k+10 + '%',
-            top: -k+10 + '%',
-        })
+            left: (-k-7) + '%',
+            top: (k-7) + '%'
+        });
         $('#game-area :nth-child(' + elementArr[k+1] + ') .front img').parent().parent().css({
-            left: -k+10 + '%',
-            top: -k+10 + '%',
-        })
-
+            left: (k-7) + '%',
+            top: (-k-7) + '%'
+        });
         setTimeout(function(){
+            $('#game-area .front').toggleClass('spinner');
             for(var i = 1; i < 19; i++) {
                 $('#game-area :nth-child(' + i + ') .front img').parent().parent().css({
                     left: 0,
@@ -191,6 +191,7 @@ function moveCard () {
                 })
             }
         }, 1500);
+        $('#game-area .front').toggleClass('spinner')
 
         $('#game-area .back').hide();
         setTimeout(function () {
@@ -201,7 +202,6 @@ function moveCard () {
 
 
 function display_stats () {
-
     $('.games-played > .value').text(games_played);
     $('.attempts .value').text(attempts);
     acc();
@@ -220,10 +220,12 @@ function reset (){
     birthday.pause();
     games_played++;
     reset_stats();
+
     $('.back').show();
     moveCard();
     $('#game-area .front').removeClass('hidden');
-    $('#game-area .front').removeClass('damn');
+    $('#game-area .front').removeClass('spinner');
+
 }
 
 
@@ -250,14 +252,3 @@ function sfxControl () {
         $('#sfx').text('SFX Off')
     }
 }
-
-
-// var frontCards = ['images/card1.png','images/card2.png','images/card3.png','images/card4.png','images/card5.png',
-//     'images/card6.png','images/card7.png','images/card8.png','images/card9.png','images/card10.png','images/card11.png',
-//     'images/card12.png','images/card13.png','images/card14.png','images/card15.png','images/card16.png','images/card17.png',
-//     'images/card18.png','images/card19.png','images/card20.png','images/card21.png','images/card22.png','images/card23.png',
-//     'images/card24.png','images/card25.png','images/card26.png','images/card27.png','images/card28.png','images/card29.png',
-//     'images/card31.png','images/card32.png','images/card33.png','images/card34.png','images/card35.png','images/card36.png',
-//     'images/card37.png','images/card38.png','images/card39.png','images/card40.png','images/card41.png','images/card42.png',
-//     'images/card43.png','images/card44.png'];
-
